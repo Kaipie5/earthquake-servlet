1b4b25c37fc8f398a7f91803ecc0ee148b83c293
Kai <mcconnka@whitman.edu>
2018-06-19 09:53:36 -0700

    Initial Commit

---
+++ src/main/java/com/stockcharts/earthquake/servlet/Main.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stockcharts.earthquake.servlet;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author kaimcconnell
+ */
+public class Main {
+    public static final String DATABASE_URL = "jdbc:mariadb:aurora://scc-intern-db"
+            + ".couiu6erjuou.us-east-1.rds.amazonaws.com:3306/InternDB?user=intern"
+            + "&password=stockcharts2018&trustServerCertificate=true&connectTimeout=5000";
+    private static final String DB_DRIVER_CLASS = "org.mariadb.jdbc.Driver";
+    public static void main(String[] args) {
+        try {
+            Class.forName(DB_DRIVER_CLASS);
+        }catch (ClassNotFoundException e){
+            System.out.println("DRIVER CLASS NOT FOUND");
+            e.printStackTrace();
+            return;
+        }
+        String query = "SELECT * FROM InternDB.Earthquakes WHERE magnitude > 5";
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL)) {
+            
+            PreparedStatement stmt = conn.prepareStatement(query);
+            
+            ResultSet rs = stmt.executeQuery();
+//            int numColumns = rs.getMetaData().getColumnCount();
+//            System.out.println(numColumns);
+//            for (int i = 1; i <= numColumns; i++) {
+//                System.out.println(rs.getMetaData().getColumnName(i));
+//            }
+            List<Earthquake> earthquakes = new ArrayList<>();
+            
+            while (rs.next()) {
+                String id = rs.getString("id");
+                float magnitude = rs.getFloat("magnitude");
+                float latitude = rs.getFloat("latitude");
+                float longitude = rs.getFloat("longitude");
+                long time = rs.getLong("time");
+                String place = rs.getString("place");
+                Earthquake newQuake = new Earthquake()
+                        .withID(id)
+                        .withLatitude(latitude)
+                        .withLongitude(longitude)
+                        .withMagnitude(magnitude)
+                        .withPlace(place)
+                        .withTime(time);
+                earthquakes.add(newQuake);
+            }
+            System.out.println(earthquakes.get(1));
+        } catch (SQLException e) {
+            System.out.println("ERROR querying database: " + e);
+        }
+    }
+}
